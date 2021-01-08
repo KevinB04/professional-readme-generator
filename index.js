79fef55ab6eceb861d456fa30b527962beb6f7d9
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-07 20:17:04 -0500

    updating files

--- index.js
+++ index.js
@@ -11,3 +11,47 @@ function init() {}
 
 // Function call to initialize app
 init();
+
+// const inquirer = require('inquirer');
+
+// inquirer
+//   .prompt([
+//     {
+//       type: "input",
+//       message: "Would you like a Table of Contents?",
+//       name: "Table of Contents",
+//     },
+//     {
+//       type: "input",
+//       message: "
+//       name: "Installation",
+//     },
+//      {
+//       type: "input",
+//       message: "
+//       name: "Credits",
+//     },
+//     {
+//       type: "input",
+//       message: "
+//       name: "License",
+//     },
+//     {
+//       type: "input",
+//       message: "
+//       name: "Badges",
+//     },
+//     {
+//       type: "input",
+//       message: "
+//       name: "Features",
+//     },
+//     {
+//       type: "input",
+//       message: "
+//       name: "Contributing",
+//     {
+//       type: "input",
+//       message: "
+//       name: "Tests",
+//     }
