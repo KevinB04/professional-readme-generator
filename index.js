04822aaff525b547dbb60ad750396872a7cc4347
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-07 20:51:09 -0500

    updating index.js

--- index.js
+++ index.js
@@ -16,42 +16,48 @@ init();
 
 // inquirer
 //   .prompt([
+//    {
+//      type: "input",
+//      message: "What is the name of your project?",
+//      name: "name",
+//    },
 //     {
 //       type: "input",
-//       message: "Would you like a Table of Contents?",
-//       name: "Table of Contents",
+//       message: "What is the description of your project?",
+//       name: "description",
 //     },
 //     {
 //       type: "input",
-//       message: "
-//       name: "Installation",
+//       message: "How can a user install your project?",
+//       name: "installation",
 //     },
 //      {
 //       type: "input",
-//       message: "
-//       name: "Credits",
+//       message: "What credits would you like to give for your project?",
+//       name: "credits",
 //     },
 //     {
 //       type: "input",
-//       message: "
-//       name: "License",
+//       message: "What license would you like to use?",
+//       name: "license",
 //     },
 //     {
 //       type: "input",
-//       message: "
-//       name: "Badges",
+//       message: "Would you like to add any badges?",
+//       name: "badges",
 //     },
 //     {
 //       type: "input",
-//       message: "
-//       name: "Features",
+//       message: "What features would you like to add?",
+//       name: "features",
 //     },
 //     {
 //       type: "input",
-//       message: "
-//       name: "Contributing",
+//       message: "Did you have any contributors on this project?",
+//       name: "contribution",
 //     {
 //       type: "input",
-//       message: "
-//       name: "Tests",
-//     }
+//       message: "How can the user test your project?",
+//       name: "tests",
+//     ],
+//   ]);
