de4df8fb2c82607166957eb41e3e3ce6212d2fea
Kevin Buttimer <kbuttimer4@gmail.com>
2021-01-08 12:29:30 -0500

    updating files

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,27 +1,47 @@
-const generateHTML = ({ name, location, bio, linkedIn, gitHub }) => {
+const generateHTML = ({ name, description, installation, credits, tests, features, contributions, badges, license, github, email }) => {
   const htmlStringToWriteToFile = `
   <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
-<html>
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title></title>
-    <meta name="description" content="">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="">
-</head>
-<body>
-    <!--[if lt IE 7]>
-        <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
-    <![endif]-->
-    
-    <script src="" async defer></script>
-</body>
-</html>
+  <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+  <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+  <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+  <!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+  <html>
+      <head>
+          <meta charset="utf-8">
+          <meta http-equiv="X-UA-Compatible" content="IE=edge">
+          <title></title>
+          <meta name="description" content="">
+          <meta name="viewport" content="width=device-width, initial-scale=1">
+          <link rel="stylesheet" href="">
+      </head>
+      <body>
+  
+          <div class="container">
+              <div class="row">
+                  <h1>Name: ${data.name}</h1>
+              <h2>Description: ${data.description}</h2>
+              <p>Installation: ${data.installation}</p>
+              <p>Credits: ${data.credits}</p>
+              <p>Tests: ${data.tests}</p>
+              <p>Features: ${data.features}</p>
+              <p>Contributions: ${data.contributions}</p>
+              <p>Badges: ${data.badges}</p>
+              <p>License: ${data.license}</p>
+              <p>GitHub: ${data.github}</p>
+              <p>Email: ${data.email}</p>
+              <a href="${data.linkedin}">LinkedIn URL</a>
+              <a href="https://github.com/${data.github}">GitHub URL</a>
+                  
+              </div>
+          </div>
+          <!--[if lt IE 7]>
+              <p class="browsehappy">You are using an <strong>outdated</strong> browser. Please <a href="#">upgrade your browser</a> to improve your experience.</p>
+          <![endif]-->
+  
+          
+          <script src="" async defer></script>
+      </body>
+  </html>
 `;
 return htmlStringToWriteToFile;
 };
